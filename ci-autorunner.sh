33b064074e739d9035ec1abecb99355940c1f5cb
Bastian Ike thebod <bastian.ike@googlemail.com>
2014-09-04 15:03:46 +0200

    added ci-autorunner.sh

---
+++ ci-autorunner.sh
@@ -0,0 +1,27 @@
+set -e
+set -x
+ 
+# check if this is a travis environment
+function cleanup {
+  echo "Removing build directory ${BUILDENV}"
+  rm -rf ${BUILDENV}
+}
+ 
+trap cleanup EXIT
+ 
+if [ ! -z $TRAVIS_BUILD_DIR ]
+then
+  WORKSPACE=$TRAVIS_BUILD_DIR
+fi
+ 
+BUILDENV=`mktemp -d`
+ 
+echo "Using build directory ${BUILDENV}"
+ 
+git clone https://github.com/AOEpeople/MageTestStand.git ${BUILDENV}
+cp -rf ${WORKSPACE} ${BUILDENV}/.modman/
+${BUILDENV}/install.sh
+ 
+cd ${BUILDENV}/htdocs
+${BUILDENV}/bin/phpunit --colors -d display_errors=1
+
