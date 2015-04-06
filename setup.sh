d79f44571dcd3b401a0079a45bf096b6d1b503af
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2015-04-07 00:22:25 +0200

    make external tools executable

--- setup.sh
+++ setup.sh
@@ -29,11 +29,17 @@ BUILDENV=`mktemp -d /tmp/mageteststand.XXXXXXXX`
 echo "Using build directory ${BUILDENV}"
  
 git clone https://github.com/ffuenf/MageTestStand.git "${BUILDENV}"
+
 mkdir ${BUILDENV}/tools
 curl -s -L https://raw.githubusercontent.com/colinmollenhour/modman/master/modman -o ${BUILDENV}/tools/modman
+chmod +x ${BUILDENV}/tools/modman
 curl -s -L https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.phar -o ${BUILDENV}/tools/n98-magerun
+chmod +x ${BUILDENV}/tools/n98-magerun
 curl -s -L https://getcomposer.org/composer.phar -o ${BUILDENV}/tools/composer
+chmod +x ${BUILDENV}/tools/composer
 curl -s -L https://phar.phpunit.de/phploc.phar -o ${BUILDENV}/tools/phploc
+chmod +x ${BUILDENV}/tools/phploc
+
 cp -rf "${WORKSPACE}" "${BUILDENV}/.modman/"
 ${BUILDENV}/install.sh
  
