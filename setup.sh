a6c04be790cc2b1b7f398be2fc4ac211593cef23
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2015-05-21 00:10:30 +0200

    integrate code-coverage via scrutinizer-ci

--- setup.sh
+++ setup.sh
@@ -27,8 +27,8 @@ echo "Using build directory ${BUILDENV}"
  
 git clone -b testing https://github.com/ffuenf/MageTestStand "${BUILDENV}"
 
-mkdir ${BUILDENV}/build/logs
-mkdir ${BUILDENV}/tools
+mkdir -p ${WORKSPACE}/build/logs
+mkdir -p ${BUILDENV}/tools
 curl -s -L https://raw.githubusercontent.com/colinmollenhour/modman/master/modman -o ${BUILDENV}/tools/modman
 chmod +x ${BUILDENV}/tools/modman
 curl -s -L https://raw.githubusercontent.com/netz98/n98-magerun/master/n98-magerun.phar -o ${BUILDENV}/tools/n98-magerun
@@ -44,20 +44,20 @@ cp -rf "${WORKSPACE}" "${BUILDENV}/.modman/"
 ${BUILDENV}/install.sh
 
 cd ${BUILDENV}/htdocs
-${BUILDENV}/bin/phpunit --coverage-clover=${BUILDENV}/build/logs/clover.xml --colors -d display_errors=1
+${BUILDENV}/bin/phpunit --coverage-text --coverage-clover=${WORKSPACE}/build/logs/clover.xml --colors -d display_errors=1
 
 if [ ! -z $CODECLIMATE_REPO_TOKEN ] ; then
   echo "Exporting code coverage results to codeclimate"
-  cd ${BUILDENV}
-  vendor/codeclimate/php-test-reporter/composer/bin/test-reporter
+  cd ${WORKSPACE}
+  ${BUILDENV}/vendor/codeclimate/php-test-reporter/composer/bin/test-reporter
 fi
 
-echo "Exporting code coverage results to scrutinizer-ci"
-cd ${BUILDENV}/build/logs
-if [ ! -z $SCRUTINIZER_ACCESS_TOKEN ] ; then
-  php -f ${BUILDENV}/tools/ocular code-coverage:upload --access-token=${SCRUTINIZER_ACCESS_TOKEN} --format=php-clover clover.xml
-else
-  php -f ${BUILDENV}/tools/ocular code-coverage:upload --format=php-clover clover.xml
-fi
+#echo "Exporting code coverage results to scrutinizer-ci"
+#cd ${BUILDENV}/build/logs
+#if [ ! -z $SCRUTINIZER_ACCESS_TOKEN ] ; then
+#  php -f ${BUILDENV}/tools/ocular code-coverage:upload --access-token=${SCRUTINIZER_ACCESS_TOKEN} --format=php-clover clover.xml
+#else
+#  php -f ${BUILDENV}/tools/ocular code-coverage:upload --format=php-clover clover.xml
+#fi
 
 echo "Done."
