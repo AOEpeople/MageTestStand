0e1e9a4b649ab03086619dfad6fcf28177d323bc
Achim Rosenhagen <a.rosenhagen@ffuenf.de>
2016-03-09 23:58:18 +0100

    rewrite travis logic

--- before_script.sh
+++ before_script.sh
@@ -10,7 +10,3 @@ if [[ -n "$GITHUB_TOKEN" ]]; then
     composer config github-oauth.github.com "$GITHUB_TOKEN"
 fi
 composer install --no-interaction --prefer-dist
-
-service postfix stop
-smtp-sink -d "%d.%H.%M.%S" localhost:2500 1000 &
-echo 'sendmail_path = "/usr/sbin/sendmail -t -i "' > ~/.phpenv/versions/$(phpenv version-name)/etc/conf.d/sendmail.ini
