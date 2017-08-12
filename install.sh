d6f117df0bcb2e8a083d209db3d3b3ae03d39d3b
Tobias Schifftner <ts@ambimax.de>
2017-08-13 00:19:46 +0200

    Added error_exit to install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,11 @@
 ABSPATH=$(cd "${0%/*}" 2>/dev/null; echo "${PWD}/${0##*/}")
 SOURCE_DIR=`dirname "${ABSPATH}"`
 
+function error_exit {
+	echo "$1" 1>&2
+	exit 1
+}
+
 if [ -z $MAGENTO_DB_HOST ]; then MAGENTO_DB_HOST="localhost"; fi
 if [ -z $MAGENTO_DB_PORT ]; then MAGENTO_DB_PORT="3306"; fi
 if [ -z $MAGENTO_DB_USER ]; then MAGENTO_DB_USER="root"; fi
@@ -34,20 +39,20 @@ if [ ! -f htdocs/app/etc/local.xml ] ; then
     # Create main database
     MYSQLPASS=""
     if [ ! -z $MAGENTO_DB_PASS ]; then MYSQLPASS="-p${MAGENTO_DB_PASS}"; fi
-    mysql -u${MAGENTO_DB_USER} ${MYSQLPASS} -h${MAGENTO_DB_HOST} -P${MAGENTO_DB_PORT} -e "DROP DATABASE IF EXISTS \`${MAGENTO_DB_NAME}\`; CREATE DATABASE \`${MAGENTO_DB_NAME}\`;"
+    mysql -u${MAGENTO_DB_USER} ${MYSQLPASS} -h${MAGENTO_DB_HOST} -P${MAGENTO_DB_PORT} -e "DROP DATABASE IF EXISTS \`${MAGENTO_DB_NAME}\`; CREATE DATABASE \`${MAGENTO_DB_NAME}\`;" || error_exit "Mysql: Drop or create database failed"
 
-    sed -i -e s/MAGENTO_DB_HOST/${MAGENTO_DB_HOST}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
-    sed -i -e s/MAGENTO_DB_PORT/${MAGENTO_DB_PORT}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
-    sed -i -e s/MAGENTO_DB_USER/${MAGENTO_DB_USER}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
-    sed -i -e s/MAGENTO_DB_PASS/${MAGENTO_DB_PASS}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
-    sed -i -e s/MAGENTO_DB_ALLOWSAME/${MAGENTO_DB_ALLOWSAME}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
+    sed -i -e s/MAGENTO_DB_HOST/${MAGENTO_DB_HOST}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit || error_exit "Setting app/etc/local.xml.phpunit failed"
+    sed -i -e s/MAGENTO_DB_PORT/${MAGENTO_DB_PORT}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit || error_exit "Setting app/etc/local.xml.phpunit failed"
+    sed -i -e s/MAGENTO_DB_USER/${MAGENTO_DB_USER}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit || error_exit "Setting app/etc/local.xml.phpunit failed"
+    sed -i -e s/MAGENTO_DB_PASS/${MAGENTO_DB_PASS}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit || error_exit "Setting app/etc/local.xml.phpunit failed"
+    sed -i -e s/MAGENTO_DB_ALLOWSAME/${MAGENTO_DB_ALLOWSAME}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit || error_exit "Setting app/etc/local.xml.phpunit failed"
 
     if [ $MAGENTO_DB_ALLOWSAME == "0" ] ; then
       # Create test database
-      mysql -u${MAGENTO_DB_USER} ${MYSQLPASS} -h${MAGENTO_DB_HOST} -P${MAGENTO_DB_PORT} -e "DROP DATABASE IF EXISTS \`${MAGENTO_DB_NAME}_test\`; CREATE DATABASE \`${MAGENTO_DB_NAME}_test\`;"
-      sed -i -e s/MAGENTO_DB_NAME/${MAGENTO_DB_NAME}_test/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
+      mysql -u${MAGENTO_DB_USER} ${MYSQLPASS} -h${MAGENTO_DB_HOST} -P${MAGENTO_DB_PORT} -e "DROP DATABASE IF EXISTS \`${MAGENTO_DB_NAME}_test\`; CREATE DATABASE \`${MAGENTO_DB_NAME}_test\`;" || error_exit "Creating test database failed"
+      sed -i -e s/MAGENTO_DB_NAME/${MAGENTO_DB_NAME}_test/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit || error_exit "Setting app/etc/local.xml.phpunit failed"
     else
-      sed -i -e s/MAGENTO_DB_NAME/${MAGENTO_DB_NAME}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit
+      sed -i -e s/MAGENTO_DB_NAME/${MAGENTO_DB_NAME}/g .modman/Aoe_TestSetup/app/etc/local.xml.phpunit || error_exit "Setting app/etc/local.xml.phpunit failed"
     fi
 
     tools/n98-magerun.phar install \
@@ -56,13 +61,13 @@ if [ ! -f htdocs/app/etc/local.xml ] ; then
       --useDefaultConfigParams=yes \
       --magentoVersionByName="${MAGENTO_VERSION}" \
       --installationFolder="${SOURCE_DIR}/htdocs" \
-      --baseUrl="http://magento.local/" || { echo "Installing Magento failed"; exit 1; }
+      --baseUrl="http://magento.local/" || error_exit "Installing Magento failed"
 fi
 
 if [ ! -f composer.lock ] ; then
-    tools/composer.phar install
+    tools/composer.phar install || error_exit "Composer install failed"
 fi
 
-tools/modman deploy-all --force
+tools/modman deploy-all --force || error_exit "Modman deployment failed"
 
-tools/n98-magerun.phar --root-dir=htdocs config:set dev/template/allow_symlink 1
+tools/n98-magerun.phar --root-dir=htdocs config:set dev/template/allow_symlink 1 || error_exit "Failed to allow symlinks configuration"
